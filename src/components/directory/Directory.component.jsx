8304b605a549d88d40ba26930f0f3bacb9574ea1
martins wasami <wasamiogheneovo@gmail.com>
2023-05-07 10:27:33 +0800

    updating our directory page with directory component and category

---
+++ src/components/directory/Directory.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CategoryItem from "../category-item/Category-item";
+import { categories } from "../../data";
+import "./categories.styles.scss";
+
+const Directory = () => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Hats",
+    img: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    img: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    img: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 1,
+    title: "Women",
+    img: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 1,
+    title: "Men",
+    img: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
